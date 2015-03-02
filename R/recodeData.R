278948e7fbae0c86c2e7e2d71291d23566aab447
Michal Modzelewski <mr-majkel@users.noreply.github.com>
2015-03-02 15:33:02 +0100

    recodeData działa teraz z nowym formatem klucza

--- R/recodeData.R
+++ R/recodeData.R
@@ -1,28 +1,38 @@
 #' Rekoduje zbiór danych zgodnie z kluczem.
 #' 
+#' @description
+#' Rekoduje zbiór danych zgodnie z kluczem. Klucz powinien być nazwaną listą.
+#' Nazwy elementów \code{key_list} powinny odpowiadać nazwom kolumn w
+#' \code{raw_df}. Dla każdej kolumny, która ma być zrekodowana, definicja
+#' wartości wyjściowych oraz docelowych powinna przyjmować postać w data.frame,
+#' z kolumnami o nazwach 'value' (wartości wyjściowe) i 'recode'
+#' (wartości docelowe).
+#' 
 #' @param raw_df data.frame z bazą do zrekodowania.
-#' @param key_list lista zawierająca .
+#' @param key_list lista z kluczem.
 #' 
 #' @return
-#' Data.frame, w którym dla kolumn wyspecyfikowanych w \code{key_list}
-#' odpowiednie wartości w 'values' zostały podmienione na wartości z 'recodes'.
-#' Kolumny nie zdefiniowane w \code{key_list} są zwracane bez zmian.
+#' Data.frame, w którym dla kolumn wymienionymi w
+#' \code{key_list} odpowiednie wartości w kolumnie 'value' zostały podmienione
+#' na wartości z kolumny 'recode'. Kolumny nie wymienione w \code{key_list} są
+#' zwracane bez zmian.
 #' 
 #' @export
 #' 
 
 recodeData = function(raw_df, key_list) {
     recoded_df = raw_df
-    items = key_list[["items"]]
+    items = names(key_list)
     
     for(it in items) {
-        it_vals = key_list[["values"]][[it]]
-        it_recodes = key_list[["recodes"]][[it]]
+        it_vals = key_list[[it]]$value
+        it_recodes = key_list[[it]]$recode
         for (v in it_vals) {
             recode_ind = which(raw_df[, it] == v)
             recoded_df[recode_ind, it] = it_recodes[which(it_vals == v)]
         }
     }
-    recoded_df[, items] = data.frame(lapply(recoded_df[, items], as.numeric))
+    # recoded_df[, items] = data.frame(recoded_df[, items],
+    #                                  stringsAsFactors = FALSE)
     recoded_df
 }
